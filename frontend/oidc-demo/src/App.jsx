adfedb8b416c75fb8fb600ef8462696b1864bbc5
Nick Anderssohn <nicholas.anderssohn@gmail.com>
2025-05-11 13:58:50 -0700

    adds login with google button

--- frontend/oidc-demo/src/App.jsx
+++ frontend/oidc-demo/src/App.jsx
@@ -6,31 +6,41 @@ import { useEffect } from 'react'
 
 function App() {
   const [userData, setUserData] = useState(null)
-  const [count, setCount] = useState(0)
+  const [loggedIn, setLoggedIn] = useState(false)
 
   useEffect(() => {
     fetch('/private/api/me', {credentials: 'include'})
       .then(response => response.json())
       .then(data => {
         setUserData(data)
+        setLoggedIn(true)
       })
       .catch(error => {
         console.error('Error fetching /private/api/me:', error)
+        setLoggedIn(false)
       })
   }, [])
 
+  var googleIdentityExists = userData &&
+          Array.isArray(userData.identities) &&
+            userData.identities.some(
+              (identity) => identity.identityProviderId === "google"
+            )
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>OpenID Connect Demo</h1>
       <div className="card">
+        {loggedIn && <h2>Logged in! Link another account:</h2>}
+        {!loggedIn && <h2>Not logged in! Please log in or create an account via:</h2>}
+        {!googleIdentityExists && (
+          <button onClick={() => window.location.href = '/login/google'}>
+            Google
+          </button>
+        )}
+        {googleIdentityExists && (
+          <p>all account types already linked, no new ones left.</p>
+        )}
         {userData && (
           <div className="user-info">
             <h2>User Info</h2>
@@ -41,16 +51,7 @@ function App() {
             />
           </div>
         )}
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
